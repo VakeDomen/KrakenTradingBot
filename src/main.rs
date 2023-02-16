f416f627ba8ac18eae91d3655c62365e1b88549d
VakeDomen <vake.domen94@gmail.com>
2023-02-16 09:31:01 +0100

    test trade executed

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
 
 use chrono::Local;
 use krakenrs::ws::{KrakenWsConfig, KrakenWsAPI, BookData};
-use krakenrs::{KrakenRestConfig, KrakenRestAPI, KrakenCredentials};
+use krakenrs::{KrakenRestConfig, KrakenRestAPI, KrakenCredentials, BsType, LimitOrder, AddOrderResponse, GetOpenOrdersResponse};
 use once_cell::sync::Lazy;
 use rust_decimal::Decimal;
 use rust_decimal::prelude::ToPrimitive;
-use std::collections::HashMap;
+use std::collections::{HashMap, BTreeSet};
+use std::io::{Error, ErrorKind};
 use std::sync::Mutex;
 use std::{
     time::Duration,
@@ -16,8 +17,8 @@ use std::{
 /**
  * Last executed order price
  */
-pub static LAST_ORDER: Lazy<Mutex<(f64, bool)>> = Lazy::new(|| {
-    match serde_any::from_file("last.data") {
+pub static LAST_ORDER: Lazy<Mutex<(f64, bool, )>> = Lazy::new(|| {
+    match serde_any::from_file("last.json") {
         Ok(hm) => Mutex::new(hm),
         Err(e) => panic!("No price history! Can't calculate gainz!: {}", e.to_string()),
     }
@@ -46,21 +47,43 @@ fn main() {
         let last = LAST_ORDER.lock().unwrap();   
         println!("{:#?}", last);
     }
-    let balance = match get_account_balance() {
+    let mut balance = match get_account_balance() {
         Some(balance) => balance,
         None => panic!("Couldn't get account balance!"),
     };
-    for (b_key, b_val) in balance.iter() {
-        println!("{}: {}", b_key, b_val);
-    }
+    let mut balance_stained = false;
+    // for (b_key, b_val) in balance.iter() {
+    //     println!("{}: {}", b_key, b_val);
+    // }
 
-    
+    let mut time_to_wait_in_millis = 1000;
 
     loop {
         /*
          * slow down the loop
          */ 
-        thread::sleep(Duration::from_millis(1000));
+        thread::sleep(Duration::from_millis(time_to_wait_in_millis));
+
+        /*
+         *  check order resolution update 
+         *  NOTE: must be above balance update, so it 
+         *  updates after order resolution
+         */
+        if is_waiting_order_resolution() {
+            time_to_wait_in_millis = 30000;
+            let orders = get_open_orders();
+            match orders {
+                Ok(ord) => {
+                    if ord.open.len() == 0 {
+                        complete_last_order();
+                        time_to_wait_in_millis = 5000;
+                    }
+                },
+                Err(e) => { println!("[{} | ORDER RESOLUTION WAIT] Could not fetch open orders: {}", time(), e.to_string())},
+            }
+            println!("[{} | ORDER RESOLUTION WAIT] Waiting for order to resolve", time());
+            continue;
+        }
 
         /*
          * update prices on tick
@@ -68,33 +91,182 @@ fn main() {
         update_prices(ws.get_all_books());
 
         /*
-         * get my balance and my position
+         * get my balance
          */
-        let balance = match get_account_balance() {
-            Some(bal) => bal,
-            None => panic!("[{} | LOOP] Error getting account balance", time()),
-        };
+        if balance_stained {
+            balance = match get_account_balance() {
+                Some(bal) => bal,
+                None => panic!("[{} | LOOP] Error getting account balance", time()),
+            };
+            balance_stained = false;
+        }
+
+        /*
+         * calc my position from balance 
+         */
         let position = get_my_position(&balance);
         println!("POSITION: {:#?}", position);
 
         /*
          * get current gain
          */
-        let gain = calculate_gain(position);
-        println!("GAIN: {}", gain);
+        let gain = match calculate_gain(&position) {
+            Some(gain) => gain,
+            None => panic!("[{} | LOOP] Error calculating gain!", time()),
+        };
+        println!("GAIN: {:#?}", gain);
 
+        /*
+         * hop strat eval
+         */
+        let should_hop = match position {
+            Position::Btc => gain > 0.02,
+            Position::Eth => gain > 0.05,
+            Position::None => false,
+        };
 
-        // print_prices();
+        /*
+         * execute hop
+         */
+        if should_hop {
+            match execute_hop(position, &balance) {
+                Ok((order_response, price)) => {
+                    println!("SLO JE!!!: {:#?}", order_response); 
+                    update_last_order(order_response, price);
+                    balance_stained = true;
+                },
+                Err(e) => panic!("PANIC: {}", e.to_string())
+            };
+        }
 
         /*
          * stop loop on stream closed 
          */
-        if ws.stream_closed() { return; }
+        if ws.stream_closed() { 
+            println!("Stream closed");
+            return; 
+        }
     }
 }
 
-fn calculate_gain(position: Position) -> f64 {
-    
+fn update_last_order(order_response: AddOrderResponse, price: f64) {
+    {
+        let mut last = LAST_ORDER.lock().unwrap();
+        last.0 = price;
+        last.1 = false;
+    }
+    save_last_order();
+}
+
+fn complete_last_order() {
+    {
+        let mut last = LAST_ORDER.lock().unwrap();
+        last.1 = true;
+    }
+    save_last_order()
+}
+
+fn get_open_orders() -> Result<GetOpenOrdersResponse, krakenrs::Error> {
+    let api = REST_API.lock().unwrap();
+    api.get_open_orders(None)
+}
+
+fn is_waiting_order_resolution() -> bool {
+    let last = LAST_ORDER.lock().unwrap();
+    !last.1
+}
+
+fn execute_hop(position: Position, balance: &HashMap<String, Decimal>) -> Result<(AddOrderResponse, f64), Error> {
+    let bs_type = match position {
+        Position::Btc => BsType::Buy,
+        Position::Eth => BsType::Sell,
+        Position::None => return Err(Error::new(
+            ErrorKind::Other, 
+            format!("[{} | EXECUTE HOP] No position specified!", time())
+        )),
+    };
+
+    let volume_option = match position {
+        Position::Btc => balance.get("XXBT"),
+        Position::Eth => balance.get("XETH"),
+        Position::None => return Err(Error::new(
+            ErrorKind::Other, 
+            format!("[{} | EXECUTE HOP] No position specified!", time())
+        )),
+    };
+
+    let volume = match volume_option {
+        Some(vol) => vol.to_f64().unwrap().to_string(),
+        None => return Err(Error::new(
+            ErrorKind::Other, 
+            format!("[{} | EXECUTE HOP] Error geting volume from balance!", time())
+        )),
+    };
+    let pair = "ETH/BTC".to_string();
+
+    let price_float = match get_currnet_relative_price() {
+        Some(pr) => round_to_precision(pr),
+        None => return Err(Error::new(
+            ErrorKind::Other, 
+            format!("[{} | EXECUTE HOP] Error geting price from state!", time())
+        )),
+    };
+    let price = price_float.to_string();
+
+    let oflags = BTreeSet::new();
+
+    let api = REST_API.lock().unwrap();
+    let limit_order = LimitOrder {
+        bs_type,
+        volume,
+        pair,
+        price,
+        oflags,
+    };
+    match api.add_limit_order(
+        limit_order, 
+        None, 
+        true
+    ) {
+        Ok(r) => Ok((r, price_float)),
+        Err(e) => Err(Error::new(
+            ErrorKind::Other, 
+            format!("[{} | EXECUTE HOP] Error executing transaction: {}", time(), e.to_string())
+        )),
+    }
+}
+
+fn round_to_precision(num: f64) -> f64 {
+    (num * 100000.0).round() / 100000.0
+}
+
+fn calculate_gain(position: &Position) -> Option<f64> {
+    let (last_value, last_completed) = get_last_trade();
+    if !last_completed {
+        return None;
+    }
+
+    if let Position::None = position {
+        return None;
+    }
+
+    let current_value = match get_currnet_relative_price() {
+        Some(val) => val,
+        None => return None,
+    };
+
+    let gain_ratio = (current_value / last_value) - 1.;
+
+    match position {
+        Position::Btc => Some(gain_ratio * -1.),
+        Position::Eth => Some(gain_ratio),
+        Position::None => return None,
+    }
+}
+
+fn get_currnet_relative_price() -> Option<f64> {
+    let prices = CURRENT_PRICES.lock().unwrap();
+    prices.2
 }
 
 fn print_prices() {
@@ -119,7 +291,6 @@ fn update_prices(books: std::collections::BTreeMap<String, BookData>) {
             Some(bid) => bid,
             None => panic!(),
         };
-        println!("{}", book_id);
         let current_price = (min_ask + max_bid) / 2.;
         update_price(book_id, current_price);
     }
@@ -180,10 +351,10 @@ fn setup_rest() -> KrakenRestAPI {
 }
 
 fn save_last_order() {
-    let mut last = LAST_ORDER.lock().unwrap();
-    match serde_any::to_file("last.data", &*last) {
+    let last = LAST_ORDER.lock().unwrap();
+    match serde_any::to_file("last.json", &*last) {
         Ok(_) => {();},
-        Err(e) => {println!("[{} | ORDER SAVE] Error saving last.data: {:#?}", time(), e);}
+        Err(e) => {println!("[{} | ORDER SAVE] Error saving last.json: {:#?}", time(), e);}
     };
 }
 
@@ -208,6 +379,11 @@ fn get_my_position(balance: &HashMap<String, Decimal>) -> Position {
     } else {
         Position::Eth
     }
+}
+
+fn get_last_trade() -> (f64, bool) {
+    let last = LAST_ORDER.lock().unwrap();
+    (last.0, last.1)
 }
 
 fn get_btc_value(ammount: f64) -> f64 {
